4a85814fe7e1a0f71fb8ec1cb90bddca91d1feed
Yassine <ellayaya01@gmail.com>
2024-05-06 17:19:30 +0200

    changment de nom

---
+++ Projet_cpp/CArbreBoruvka.h
@@ -0,0 +1,40 @@
+#ifndef __CBoruvka__
+#define __CBoruvka__					
+
+#include "CGrapheOriente.h"
+using namespace std;
+
+
+/************************************************************
+* CLASSE : Classe outils pour la creation d'un arbre couvrant
+* de poids minimal
+* ************************************************************
+* ROLE : Interface de la classe CBoruvka,
+* cette classe fournit un ensemble de service pour la realisation
+* d'un arbre couvrant de poids minimal a partir de l'algo de 
+* Boruvka
+* ************************************************************
+* VERSION :
+* AUTEUR : Yassine ELLATIFI / Milan HERGOTT
+* DATE : 06/05/2024
+* ************************************************************
+* INCLUSIONS EXTERNES :
+*/
+
+/* TYPES :
+*  pas de types particulier de déclaré
+*/
+
+/* VARIABLES :
+*
+*
+*/
+
+
+
+class CArbreBoruvka
+{
+	CGrapheOriente* pGROABKGraphParam;
+};
+
+#endif
