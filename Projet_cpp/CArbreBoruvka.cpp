9ddd65a6d87da389044cbd30fb2fda34bbae6979
Yassine <ellayaya01@gmail.com>
2024-05-18 12:11:10 +0200

    on continue boruvka ajout des methode SommetPresent et GRODecomposeSom

--- Projet_cpp/CArbreBoruvka.cpp
+++ Projet_cpp/CArbreBoruvka.cpp
@@ -62,17 +62,37 @@ CArcPondere* CArbreBoruvka::ABKMinPoids(list<CArcPondere*> ListParam) {
 * Entraîne : creation de l'arbre couvrant minimal a partir
 * de l'algorithme Boruvka
 * ****************************************************/
-/*
-PGrapheOriente* CArbreBoruvka::ABKBoruvka() {
+
+PGrapheOriente<CArcPondere,PSommet<CArcPondere>>* CArbreBoruvka::ABKBoruvka() {
 	unsigned int uiBoucle;
-	PGrapheOriente* pGROArbreCouvrant = new PGrapheOriente();
-	PGrapheOriente* pGROGrapheTravail = new PGrapheOriente(*pGROABKGraphParam);
+	PSommet<CArcPondere > *pSOMTemp;
+	list<CArcPondere*> lARCTemp;
+	CArcPondere* pARCMin;
+	PGrapheOriente<CArcPondere, PSommet<CArcPondere>>* pGROArbreCouvrant = new PGrapheOriente<CArcPondere, PSommet<CArcPondere>>();
+	PGrapheOriente<CArcPondere, PSommet<CArcPondere>>* pGROGrapheTravail = new PGrapheOriente<CArcPondere, PSommet<CArcPondere>>(*pGROABKGraphParam);
 	while (pGROGrapheTravail->GRONombreSommet() > 1) {
 		//elimination arc reflexif et multiple
-		for (uiBoucle = 0; uiBoucle < pGROABKGraphParam->GRONombreSommet(); uiBoucle++) {
-			pGROABKGraphParam->GROLireSommet(uiBoucle);
+		for (uiBoucle = 0; uiBoucle < pGROGrapheTravail->GRONombreSommet(); uiBoucle++) {
+			pSOMTemp = pGROGrapheTravail->GROLireSommet(uiBoucle);  //on recupere un sommet contenu dans le graphe de travail
+			for (unsigned int uiBoucle2 = 0; uiBoucle2 < pSOMTemp->SOMTaileListArcSortant(); uiBoucle2++) {
+				lARCTemp.push_back(pSOMTemp->SOMLireElemListArcEntrant(uiBoucle2)); // creation de la liste des Arc sortant d'un sommet.
+			}
+			pARCMin = ABKMinPoids(lARCTemp);  // on recupere l'arc de poids minimal de la liste
+
+			//objectif determiner si les sommet de cette arc sont une agregation de sommet, un sommet unique present de le graphe initial
+			// on verifie l'existence des sommets de depart et d'arrivee dans le graphe initial
+			if (pGROABKGraphParam->GROSommetPresent(pARCMin->ARCLireArrive()) == false) {  // on verifie si notre sommet est un sommet agregé si oui on le decompose
+				//fonction de decomposition du nom
+				// en cours de creation dans PGrapheOriente
+			}
+			else if (pGROArbreCouvrant->GROSommetPresent(pARCMin->ARCLireArrive()) == false) {
+				pGROArbreCouvrant->GROCreerSommet(pARCMin->ARCLireArrive()); // faux a developper encore
+			}
+			
+			//fusion dans le graphe de travail
+			//objectif fusionner les arbre
+			
 		}
 	}
 }
 
-*/
